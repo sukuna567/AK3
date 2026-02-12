50be9993f751e1e83f63a1fa41b6a2cad272e4d8
sukuna567 <bhudev12364@gmail.com>
2026-02-12 20:12:44 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string= Stormbreaker-Kernel By FACKJONNY
+kernel.string= Esport-Edition-NoMercy-Kernel By Sukuna567
 do.devicecheck=1
 do.modules=0
 do.systemless=1
